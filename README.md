621fdb53d819a84cb84cc289bba768f3b5c526f6
Tanner Silva <t@tannersilva.com>
2022-10-26 13:17:51 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cblake2
+
+This is a Swift Package that wraps the blake2 family of hashing algorithms.
+
+### License
+
+This Swift Package is available for open use under the MIT license.
---
+++ Tests/Cblake2Tests/Cblake2Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Cblake2
+
+final class Cblake2Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Cblake2().text, "Hello, World!")
+    }
+}
